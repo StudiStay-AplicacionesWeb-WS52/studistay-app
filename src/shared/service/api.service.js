9789f70ff63e7d0a20844372b2d9695deb5d8718
J. Ortega <jamutaqortegavelez@gmail.com>
2023-10-25 19:24:26 -0500

    hotfix: baseURL

--- src/shared/service/api.service.js
+++ src/shared/service/api.service.js
@@ -1,34 +1,30 @@
-import axios from "axios";
+import axios from 'axios'
 
 const http = axios.create({
-  baseURL: `${
-    import.meta.env.VITE_ENV === 'prod'
-      ? 'https://studistay.onrender.com/api/v1'
-      : 'http://localhost:5000/api/v1'
-  }`,
+  baseURL: 'https://studistay.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
-});
+})
 
 export class StudiStayApiService {
-	getAllPosts() {
-    return http.get('/post');
+  getAllPosts() {
+    return http.get('/post')
   }
 
   getPostById(id) {
-    return http.get(`/post/${id}`);
+    return http.get(`/post/${id}`)
   }
 
   createPost(data) {
-    return http.post('/post', data);
+    return http.post('/post', data)
   }
 
   deletePost(id) {
-    return http.delete(`/post/${id}`);
+    return http.delete(`/post/${id}`)
   }
 
   getAllUniversities() {
-    return http.get('/university');
+    return http.get('/university')
   }
 }
