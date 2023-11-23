8d6a58b38ac68db932e2c6d35a11df1b9a92aad2
J. Ortega <jamutaqortegavelez@gmail.com>
2023-11-22 20:22:32 -0500

    refactor: register-card component
    Co-Authored-By: DiegoLlamozas <134030499+DiegoLlamozas@users.noreply.github.com>

--- src/security/pages/register/components/register-card.component.vue
+++ src/security/pages/register/components/register-card.component.vue
@@ -1,68 +1,131 @@
 <script>
 export default {
   name: "register-card",
+  data() {
+    return {
+      user: {},
+      submitted: false,
+    }
+  },
+  methods: {
+    register() {
+      this.submitted = true;
+      if (this.user.fullName && this.user.email && this.user.phone && this.user.password) {
+        this.$securityApiService.signUp(this.user)
+          .then((response) => {
+            console.log(response)
+            this.$toast.add({
+              severity: 'success',
+              summary: 'Successful',
+              detail: 'User Created',
+              life: 3000
+            });
+            this.user = {};
+            this.$router.push({ name: 'login' });
+          })
+          .catch((e) => {
+            this.$toast.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: `Error al crear usuario: ${e.message}`,
+              life: 3000
+            });
+          }).finally(() => {
+            this.submitted = false;
+          })
+      }
+    },
+  }
 }
 
 </script>
 
 <template>
   <br><br>
-  <div class="card">
-    <pv-card>
-      <template #title> {{$t('register-view.register')}} </template>
+  <div class="centered-container">
+    <pv-card class="p-fluid" style="max-width: 90%;">
+      <template #title> 
+        <span class="paragraph font-bold"> {{$t('register-view.register')}}</span>   
+      </template>
       <template #content>
-        <p>
-          {{$t('register-view.names')}}
-        </p>
-        <div class="card flex flex-wrap justify-content-center gap-3">
-          <span class="p-input-icon-left">
-            <i class="pi pi-user"/>
-            <pv-input-text :placeholder="$t('register-view.enter-names')" ></pv-input-text>
-          </span>
-        </div>
-        <p>
-          {{$t('register-view.email')}}
-        </p>
-        <div class="card flex flex-wrap justify-content-center gap-3">
-          <span class="p-input-icon-left">
-            <i class="pi pi-envelope"/>
-            <pv-input-text :placeholder="$t('register-view.enter-email')"></pv-input-text>
-          </span>
-        </div>
-        <p>
-          {{$t('register-view.phone')}}
-        </p>
-        <div class="card flex flex-wrap justify-content-center gap-3">
-          <span class="p-input-icon-left">
-            <i class="pi pi-phone"/>
-            <pv-input-text :placeholder="$t('register-view.enter-phone')"></pv-input-text>
-          </span>
-        </div>
-        <p>
-          {{$t('register-view.password')}}
-        </p>
-        <div class="card flex flex-wrap justify-content-center gap-3">
-          <span class="p-input-icon-left">
-            <i class="pi pi-lock"/>
-            <pv-input-text :placeholder="$t('register-view.enter-password')"></pv-input-text>
+        <div class="field paragraph mt-3">
+          <span class="p-input-icon-left p-float-label">
+            <i class="pi pi-user"></i>
+            <pv-input-text 
+              type="text"
+              id="fullName"
+              v-model.trim="user.fullName"
+              required="true"
+              autofocus
+              :class="{ 'p-invalid': submitted && !user.fullName }"
+              :placeholder="$t('register-view.enter-names')" 
+            />
+            <label for="fullName">{{$t('register-view.names')}}</label>
+            <small class="p-error" v-if="submitted && !user.fullName">Ingrese su nombre completo</small>
           </span>
         </div>
 
-        <br><br>
+        <div class="field paragraph">
+          <span class="p-input-icon-left p-float-label">
+            <i class="pi pi-envelope"></i>
+            <pv-input-text 
+              type="text"
+              id="email"
+              v-model.trim="user.email"
+              required="true"
+              autofocus
+              :class="{ 'p-invalid': submitted && !user.email }"
+              :placeholder="$t('register-view.enter-email')" 
+            />
+            <label for="email">{{$t('register-view.email')}}</label>
+            <small class="p-error" v-if="submitted && !user.email">Ingrese su email</small>
+          </span>
+        </div>
+
+        <div class="field paragraph">
+          <span class="p-input-icon-left p-float-label">
+            <i class="pi pi-phone"></i>
+            <pv-input-text 
+              type="text"
+              id="phone"
+              v-model.trim="user.phone"
+              required="true"
+              autofocus
+              :class="{ 'p-invalid': submitted && !user.phone }"
+              :placeholder="$t('register-view.enter-phone')" 
+            />
+            <label for="phone">{{$t('register-view.phone')}}</label>
+            <small class="p-error" v-if="submitted && !user.phone">Ingrese su teléfono</small>
+          </span>
+        </div>
+
+        <div class="field paragraph">
+          <span class="p-input-icon-left p-float-label">
+            <i class="pi pi-lock"></i>
+            <pv-input-text 
+              type="password"
+              id="password"
+              v-model.trim="user.password"
+              required="true"
+              autofocus
+              :class="{ 'p-invalid': submitted && !user.password }"
+              :placeholder="$t('register-view.enter-password')" 
+            />
+            <label for="password">{{$t('register-view.password')}}</label>
+            <small class="p-error" v-if="submitted && !user.password">Ingrese su conraseña</small>
+          </span>
+        </div>
+
+        <br>
 
         <div class="card flex justify-content-center">
-          <router-link to="/" custom v-slot="{ navigate, href }">
-            <pv-button
-                :label="$t('register-view.register-btn')"
-                class="button-primary"
-                :href="href"
-                @click="navigate"
-            />
-          </router-link>
+          <pv-button
+            :label="$t('register-view.register-btn')"
+            class="button-secondary"
+            @click="register"
+            icon="pi pi-user-plus"
+          />
         </div>
-
-
-
       </template>
     </pv-card>
 
@@ -70,11 +133,10 @@ export default {
 </template>
 
 <style>
-
-.card {
-  text-align: center;
-  align-items: center;
-  position: relative;
-}
-
+  .centered-container {
+    text-align: center;
+    display: flex; /* Usa Flexbox */
+    justify-content: center; /* Centra horizontalmente */
+    align-items: center; /* Centra verticalmente */
+  }
 </style>
